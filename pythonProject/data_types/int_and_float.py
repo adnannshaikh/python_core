754fc5696755b0a5c985f76ea9868f7faa125299
Adnan Shaikh <sikadnan2@gmail.com>
2025-05-26 16:58:29 +0530

    first commit

---
+++ pythonProject/data_types/int_and_float.py
@@ -0,0 +1,38 @@
+# print(2 + 4)
+# print(type(2 - 4))
+# print(2 * 4)
+# print(type(2 / 4))
+#
+#
+# print(type(0))
+# print(type(0.7))
+#
+# print(2 ** 3) #Power
+# print(10 // 8) #Rounds the Quotient
+# print(10 % 3) # Returns the remainder
+
+# Math Functions
+# print(round(3.5))
+# print(abs(-33)) #Absolute function(no negative)
+
+# Operator precedence
+# (),**,* /,+ -
+
+#complex - data type (real number)
+# binary -> bin()
+
+print(bin(5))
+print(int("0b101",2))
+
+#---------------------
+a,b,c = 10,22,33
+
+print(a)
+print(b)
+print(c)
+
+# Augmented assignment operator
+some_value = 6
+# some_value = some_value + 2 -> AAO
+some_value += 2
+print(some_value)
